3899d59f75e29858ad1a26e559abd5e90d40f8f9
Basix <basix@basix.tech>
2021-04-05 23:19:41 +0900

    Circumvent CRA env restrictions

--- src/common/index.ts
+++ src/common/index.ts
@@ -3,10 +3,10 @@ import { createContext } from "react"
 let production = process.env.NODE_ENV === 'production';
 let uri;
 
-if(process.env.URL) {
+if(process.env.REACT_APP_URL) {
   // https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
   // Required for Netlify Deploy Preview to perform correctly.
-  uri = process.env.URL
+  uri = process.env.REACT_APP_URL
 }
 else if(production) {
   uri = 'https://uma.hitagi.moe'
